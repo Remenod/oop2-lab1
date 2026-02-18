74398320fb2d6e224038ec44c7eb43de785aeaf3
Remenod <remenod@gmail.com>
2026-02-18 22:13:33 +0200

    do task 2 properly

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,14 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <functional>
+
+std::vector<int> Where(const std::vector<int>& arr, std::function<bool(int)> pred) {
+    std::vector<int> result;
+    for (int x : arr) {
+        if (pred(x))
+            result.push_back(x);
+    }
+    return result;
+}
+
+int* WhereManual(int* arr, int length, std::function<bool(int)> pred, int& outLength) {
+    int count = 0;
+    for (int i = 0; i < length; ++i)
+        if (pred(arr[i]))
+            ++count;
+
+    outLength = count;
+    int* result = new int[count];
+
+    int idx = 0;
+    for (int i = 0; i < length; ++i)
+        if (pred(arr[i]))
+            result[idx++] = arr[i];
+
+    return result;
+}
 
 int main() {
-	std::vector<int> v = {1, 2, 3, 4, 5};
-	int k = 3;
+    int k;
+    std::cout << "Enter k: ";
+    std::cin >> k;
 
-	auto filtered = v
-    | std::views::filter([](int x){ return x % k == 0; });
+    int n;
+    std::cout << "Enter Arr Elements count: ";
+    std::cin >> n;
 
+    int* arr = new int[n];
 
-	for (int x : filtered)
-    	std::cout << x << " ";
+    std::cout << "Enter " << n << " numbers:\n";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> arr[i];
+    }
+
+    std::function<bool(int)> pred = [k](int x){ return x % k == 0; };
+
+    // lib method
+    std::vector<int> filtered = Where({arr, arr+n}, pred);
+    std::cout << "Filtered with lib: ";
+    for (int x : filtered) std::cout << x << " ";
+    std::cout << "\n";
+
+    // manual method
+    int manualLength;
+    int* manualResult = WhereManual(arr, n, pred, manualLength);
+    std::cout << "Filtered manual: ";
+    for (int i = 0; i < manualLength; ++i)
+        std::cout << manualResult[i] << " ";
+    std::cout << "\n";
+
+    delete[] manualResult;
+    delete[] arr;
 }
